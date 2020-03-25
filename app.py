fa07b9695ed3fb20c496223e9e0218046879ce4c
Sashko <burdin009@gmail.com>
2020-03-25 16:54:08 +0300

    data filled

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_admin.contrib.sqla import ModelView
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from models import db, Location
+from models import db, Location, Event
 
 db.init_app(app)
 db.create_all(app=app)
@@ -16,8 +16,10 @@ db.create_all(app=app)
 
 admin = Admin(app=app)
 admin.add_view(ModelView(Location, db.session))
+admin.add_view(ModelView(Event, db.session))
 
 from views import *
+from servis_views import *
 
 if __name__ == "__main__":
 
--- servis_views.py
+++ servis_views.py
@@ -1,5 +1,5 @@
 from flask import abort, flash, session, redirect, request, render_template
-from models import Location
+from models import Location, Event
 from app import app, db
 import csv
 
@@ -13,7 +13,7 @@ def locat_add():
                 continue
             else:
                 print(row)
-                locat = Location(title=int(row[1]), code=row[0])
+                locat = Location(title=row[1], code=row[0])
                 db.session.add(locat)
         db.session.commit()
 
@@ -21,3 +21,40 @@ def locat_add():
     with open(csv_path, "r") as f_obj:
         csv_reader(f_obj)
     return "Filled"
+
+
+@app.route("/events")
+def events_add():
+    def csv_reader(file_obj):
+        reader = csv.reader(file_obj)
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            else:
+                print(row[6])
+                location = db.session.query(Location).filter(Location.code == row[6].split(", ")[0]).first()
+                event = Event(title=row[0], description=row[1], date=row[2], time=row[3], type=row[4], category=row[5],
+                              location=location, location_id=location.id, seats=row[8], address=row[7])
+                db.session.add(event)
+        db.session.commit()
+
+    csv_path = "meetup_events.csv"
+    with open(csv_path, "r") as f_obj:
+        csv_reader(f_obj)
+    return "Filled"
+
+"""
+title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+    time = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    location = relationship("Location", back_populates="events")
+    location_id = Column(Integer, ForeignKey("locations.id"))
+    seats = Column(Integer, nullable=False)
+    address = Column(String, nullable=False)
+    participants = relationship(
+        "Participant", secondary=Enrollment, back_populates="events"
+    )
+"""
